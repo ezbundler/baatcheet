c68935c537956d6c054d734a8c3682dd471a4799
ezbundler <developer.akash1236@gmail.com>
2025-01-31 02:58:23 +0530

    backend setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.route.js";
-
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
 app.use("/api/auth",authRoutes);
